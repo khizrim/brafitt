f6681ed7db936dd66d951ae2267172b0b2ceaf01
Khizri Makhmudov <kh.makhmudov@me.com>
2021-11-20 12:49:04 +0300

    feat: add Header to the page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,14 @@ import React from 'react';
 import Meta from '../components/Meta/Meta';
 import Layout from '../components/Layout/Layout';
 
+import Header from '../components/Header/Header';
+
 const IndexPage = () => {
   return (
     <>
+      <Meta />
+      <Header />
       <Layout>
-        <Meta />
         <p className="container">
           There will be the landing page for the first brafitting service in
           Makhachkala, Russia! See you soon! &#128521;
