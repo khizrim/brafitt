c6662c4250128070f8ae272c82423b677d47741a
Khizri Makhmudov <kh.makhmudov@me.com>
2021-11-20 17:11:19 +0300

    feat: create Features

---
+++ src/components/Features/Features.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import Section from '../Section/Section';
+import Feature from '../Feature/Feature';
+
+import './Features.css';
+
+const Features = ({ data }: FeaturesProps) => {
+  const { title, subtitle, features } = data;
+
+  return <Section sectionTitle="features">
+    <h2 className="features__title">{title}</h2>
+    {subtitle && <p className="features__subtitle">{subtitle}</p>}
+    <div className="features__list">
+      {features.map((feature) => {
+        return (
+          <Feature
+            key={feature._id}
+            icon={feature.icon}
+            caption={feature.caption}
+          />
+        );
+      })}
+    </div>
+  </Section>;
+};
+
+type FeaturesProps = {
+  data: {
+    title: string;
+    subtitle?: string;
+    features: Array<{
+      _id: number;
+      icon: JSX.Element;
+      caption: string;
+    }>;
+  };
+};
+
+export default Features;
