aa5f8bbba316ba01c909fcc69a35bcff628de80e
Khizri Makhmudov <kh.makhmudov@me.com>
2021-11-22 19:57:02 +0300

    feat: add features section to layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,12 @@ import Meta from '../components/Meta/Meta';
 import Layout from '../components/Layout/Layout';
 
 import Header from '../components/Header/Header';
+import Features from '../components/Features/Features';
+import Steps from '../components/Steps/Steps';
 import Promo from '../components/Promo/Promo';
 import Footer from '../components/Footer/Footer';
-import Steps from '../components/Steps/Steps';
+
+import { BENEFITS, SAFETY } from '../utils/constants';
 
 const IndexPage = () => {
   return (
@@ -14,6 +17,8 @@ const IndexPage = () => {
       <Meta />
       <Header />
       <Layout>
+        <Features data={BENEFITS} />
+        <Features data={SAFETY} />
         <Steps />
         <Promo />
       </Layout>
